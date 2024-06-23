04488190db8fc3539e530b81b52891b0e73ea0f5
sammy wlodawski <samwlodawski@gmail.com>
2024-06-23 17:49:46 -0400

    Adding relevant api fetch call info.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const searchbtn= document.getElementById('search-btn');
 
 function getApi() {
     // fetch request gets a list of all the repos for the node.js organization
-    const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={b792be78f2bfe310ca378c0e295b739b}';
+    const requestUrl = '`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b792be78f2bfe310ca378c0e295b739b}`';
   
     fetch(requestUrl)
       .then(function (response) {
