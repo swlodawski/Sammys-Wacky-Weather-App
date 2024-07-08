97bc19bf0878c48b6272bc4200396d5e27440310
sammy wlodawski <samwlodawski@gmail.com>
2024-07-08 13:14:59 -0400

    Adding global variables and localstorage functionality.

--- script.js
+++ script.js
@@ -4,22 +4,40 @@ const weatherCardDiv = document.getElementById('weather-card-div');
 const searchBtn= document.getElementById('search-btn');
 const cityHistory = JSON.parse(localStorage.getItem('cities')) || [];
 const apiKey = '50d9fa3c2b681d0729030adef69fc4b6';
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    const requestUrl = '`https://api.openweathermap.org={}`';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)   
-    });
+
+
+function updateLocStorage(cityName) {
+  if(!cityHistory.includes(cityName)) {
+    cityHistory.push(cityName);
+    localStorage.setItem('cities', JSON.stringify(cityHistory));
+    render.cityHistory();
+  }
 }
+
+// function getApi() {
+//     // fetch request gets a list of all the repos for the node.js organization
+//     const requestUrl = '`https://api.openweathermap.org={}`';
+  
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         console.log(data)   
+//     });
+// }
+
+
+
 
 const createCityHistory = function(data) {
 
 }
+
+
+
+
+
 
 const createWeatherDiv = function(data) {
    const weatherContainer = document.createElement('divContainer');
