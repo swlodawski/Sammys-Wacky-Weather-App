5389d73397048b8ae8f747056ffb7e8120b27b32
sammy wlodawski <samwlodawski@gmail.com>
2024-07-11 14:57:40 -0400

    Adding more logic and html elements.

--- script.js
+++ script.js
@@ -1,81 +1,83 @@
-const asideDiv = document.getElementById('aside-div');
-const weatherContainerDiv = document.getElementById('weather-container-div');
-const weatherCardDiv = document.getElementById('weather-card-div');
-const searchBtn= document.getElementById('search-btn');
+// const asideDiv = document.getElementById('aside-div');
+const searchCity= document.getElementById('search-button');
+const apiKey = '4f2cf59f02cfa6f47c2a9442e626336f';
 const citiesArray = JSON.parse(localStorage.getItem('cities')) || [];
 const cityHistory = document.getElementById('city-history');
-const apiKey = '50d9fa3c2b681d0729030adef69fc4b6';
+const currentWeather = document.getElementById('current-weather');
+const weeklyForecast = document.getElementById('week-forecast');
 
 
-function updateLocStorage(cityName) {
-  if(!cityHistory.includes(cityName)) {
-    cityHistory.push(cityName);
-    localStorage.setItem('cities', JSON.stringify(cityHistory));
-    render.cityHistory();
+function updateLocalStorage(cityName) {
+  if(!citiesArray.includes(cityName)) {
+    citiesArray.push(cityName);
+    localStorage.setItem('cities', JSON.stringify(citiesArray));
+    rendercityHistory();
   }
 }
 
 function getApi(cityName = null) {
 
   if(!cityName) {
-    cityName = document.getElementById('searchBtn').value;
+    cityName = document.getElementById('search-button').value;
 
   }
-    const requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q${cityName}&appid=${apiKey}`;
-    updateLocStorage(cityName);
+    const requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+    updateLocalStorage(cityName);
 
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
+      .then(response => response.json())
+      .then(data => {
         const latitude = data[0].lat;
-        const longitude = data[0].long;
+        const longitude = data[0].lon;
         getForecast(latitude, longitude, cityName);
     });
 }
 
 function getForecast (lat, lon, cityName) {
-  const requestUrlCelcius = `https://api.openweathermap.org/data/2.5/forcast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  const requestUrlFarenheit = `https://api.openweathermap.org/data/2.5/forcast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  const requestUrlCelcius = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  const requestUrlFarenheit = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
   Promise.all([
     fetch(requestUrlCelcius).then(response => response.json()),
     fetch(requestUrlFarenheit).then(response => response.json())
   ]).then(([dataCelcius, dataFarenheit]) => {
-    renderCurrentWeather(dataFarenheit, dataCelcius, cityName);
-    renderWeeklyHistory(dataFarenheit, dataCelcius, cityName);
+    renderCurrentWeather(dataCelcius, dataFarenheit, cityName);
+    renderWeeklyHistory(dataCelcius, dataFarenheit);
   });
 }
 
-function renderCurrentWeather(currentWeatherFarenheit, currentWeatherCelcius, cityName) {
-  displayCurrentWeather.innerHTML = '';
+function renderCurrentWeather(currentWeatherCelcius, currentWeatherFarenheit, cityName) {
+  currentWeather.innerHTML = '';
 
-  const weatherContainer = document.createElement('divContainer');
+  const weatherContainer = document.createElement('div');
   weatherContainer.setAttribute('class', 'border');
-  const cityDate = document.createElement('h2');
+  const cityNameEl = document.createElement('h2');
   const cityTemp = document.createElement('p');
   const cityWind = document.createElement('p');
   const cityHumidity = document.createElement('p');
 
+  cityNameEl.textContent = cityName;
+  cityTemp.textContent = `Temp: ${currentWeatherCelcius.list[0].main.temp} °C / ${currentWeatherFarenheit.list[0].main.temp} °F`;
+  cityWind.textContent = `Wind: ${currentWeatherCelcius.list[0].wind.speed} m/s/ ${currentWeatherFarenheit.list[0].main.speed} mph`;
+  cityHumidity.textContent = `Humidity: ${currentWeatherCelcius.list[0].main.humidity}`;
 
   weatherContainer.append(cityDate);
   weatherContainer.append(cityTemp);
   weatherContainer.append(cityWind);
   weatherContainer.append(cityHumidity);
 
-  weatherContainerDiv.append(weatherContainer);
+  currentWeather.append(weatherContainer);
 }
 
 
-function renderWeeklyWeather(forcastFarenheit, forcastCelcius) {
+function renderWeeklyWeather(forecastCelcius, forecastFarenheit) {
   weeklyForecast.innerHTML = '';
 
-  const dates = [...new Set(forcastCelcius.list.map(item => item.dt_txt.split('')[0]))].slice(0,5);
+  const dates = [...new Set(forecastCelcius.list.map(item => item.dt_txt.split('')[0]))].slice(0,5);
 
   dates.forEach(date => {
-    const dailyForecastCelcius = forcastCelcius.list.find(item => item.dt_txt.startsWith(date));
-    const dailyForecastFarenheit = forcastFarenheit.list.find(item => item.dt_txt.startsWith(date));
+    const dailyForecastCelcius = forecastCelcius.list.find(item => item.dt_txt.startsWith(date));
+    const dailyForecastFarenheit = forecastFarenheit.list.find(item => item.dt_txt.startsWith(date));
 
     const weatherCard = document.createElement('div');
     weatherCard.setAttribute('class', 'card');
@@ -84,17 +86,17 @@ function renderWeeklyWeather(forcastFarenheit, forcastCelcius) {
     const cityWind = document.createElement('p');
     const cityHumidity = document.createElement('p');
   
-    dayOfWeek.textContent = dayjs(date).format('dddd');
-    temperature.textContent = `Temp: ${dailyForecastCelcius.main.temp} C/ ${dailyForecastFarenheit.main.temp} F`;
-    wind.textContent = `Wind: ${dailyForecastCelcius.wind.speed} m/s ${dailyForecastFarenheit.wind.speed} mph`;
-    humidity.textContent = `Humidity: ${dailyForecastCelcius.main.humidity} %`;
+    cityDate.textContent = dayjs(date).format('dddd');
+    cityTemp.textContent = `Temp: ${dailyForecastCelcius.main.temp} °C / ${dailyForecastFarenheit.main.temp} °F`;
+    cityWind.textContent = `Wind: ${dailyForecastCelcius.wind.speed} m/s / ${dailyForecastFarenheit.wind.speed} mph`;
+    cityHumidity.textContent = `Humidity: ${dailyForecastCelcius.main.humidity} %`;
     
-    weatherCard.append(dayOfWeek);
+    weatherCard.append(cityDate);
     weatherCard.append(cityTemp);
     weatherCard.append(cityWind);
     weatherCard.append(cityHumidity);
   
-    weatherCardDiv.append(weatherCard); 
+    weeklyForecast.append(weatherCard); 
   });
 }
 
@@ -110,10 +112,10 @@ function renderSearchHistory() {
       getApi(city);
     });
    cityHistory.append(buttonEl);
-  })
+  });
 }
 
 renderSearchHistory();
 
 
-searchBtn.addEventListener('click', () => getApi());
+searchCity.addEventListener('click', () => getApi());
